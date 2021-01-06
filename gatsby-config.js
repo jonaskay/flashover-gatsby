c960d9956cd17303c68ce26fb57302d176297cc0
Joonas Kykkänen <jonaskay@iki.fi>
2021-01-06 20:16:51 +0200

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,6 +64,13 @@ module.exports = {
         icon: `src/images/flashover-icon.png`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-116599641-4`,
+        anonymize: true,
+      },
+    },
     `gatsby-plugin-postcss`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
