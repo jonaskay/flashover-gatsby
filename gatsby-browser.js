0efb5f202f36389aadb371a8beab4b79c5023966
Joonas Kykkänen <jonaskay@iki.fi>
2021-01-10 11:05:13 +0200

    Install Fontsource

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,7 @@
+import "@fontsource/crimson-pro/400.css"
+import "@fontsource/crimson-pro/400-italic.css"
+import "@fontsource/crimson-pro/600.css"
+import "@fontsource/cutive-mono/400.css"
+import "@fontsource/inter/400.css"
+
 import "./src/styles/global.css"
