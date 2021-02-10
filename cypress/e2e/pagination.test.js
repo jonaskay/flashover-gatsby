0d8d8983aca21a6c3b5f6be7aa1a8d7629c384e5
Joonas Kykkänen <jonaskay@iki.fi>
2021-02-10 13:19:17 +0200

    Test pagination and add missing nav tag

---
+++ cypress/e2e/pagination.test.js
@@ -0,0 +1,32 @@
+/// <reference types="Cypress" />
+
+describe("Pagination tests", () => {
+  beforeEach(() => {
+    cy.visit("/archive")
+  })
+
+  it("paginates archived posts", () => {
+    cy.get("main")
+      .findByLabelText("page navigation")
+      .within(() => {
+        cy.get("a").findByText("1").should("have.class", "active")
+        cy.get("a").findByText("2").should("not.have.class", "active")
+        cy.get("a").findByTitle("Next").parent().click()
+      })
+
+    cy.get("main")
+      .findByLabelText("page navigation")
+      .within(() => {
+        cy.get("a").findByText("1").should("not.have.class", "active")
+        cy.get("a").findByText("2").should("have.class", "active")
+        cy.get("a").findByTitle("Previous").parent().click()
+      })
+
+    cy.get("main")
+      .findByLabelText("page navigation")
+      .within(() => {
+        cy.get("a").findByText("1").should("have.class", "active")
+        cy.get("a").findByText("2").should("not.have.class", "active")
+      })
+  })
+})
--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -24,28 +24,30 @@ const Pagination: React.FC<PaginationProps> = ({
   }
 
   return (
-    <Container className="my-8 sm:my-16 text-center text-lg">
-      {currentPage > 1 && (
-        <PaginationLink to={pagePath(currentPage - 1)} direction="prev" />
-      )}
-      {new Array(pageCount).fill(undefined).map((_, index) => {
-        const pageNumber = index + 1
+    <nav aria-label="page navigation">
+      <Container className="my-8 sm:my-16 text-center text-lg">
+        {currentPage > 1 && (
+          <PaginationLink to={pagePath(currentPage - 1)} direction="prev" />
+        )}
+        {new Array(pageCount).fill(undefined).map((_, index) => {
+          const pageNumber = index + 1
 
-        return (
-          <Link
-            key={pageNumber}
-            to={pagePath(pageNumber)}
-            className="inline-block align-middle px-2 w-7 text-indigo-600 hover:underline"
-            activeClassName="active"
-          >
-            {pageNumber}
-          </Link>
-        )
-      })}
-      {pageCount > currentPage && (
-        <PaginationLink to={pagePath(currentPage + 1)} direction="next" />
-      )}
-    </Container>
+          return (
+            <Link
+              key={pageNumber}
+              to={pagePath(pageNumber)}
+              className="inline-block align-middle px-2 w-7 text-indigo-600 hover:underline"
+              activeClassName="active"
+            >
+              {pageNumber}
+            </Link>
+          )
+        })}
+        {pageCount > currentPage && (
+          <PaginationLink to={pagePath(currentPage + 1)} direction="next" />
+        )}
+      </Container>
+    </nav>
   )
 }
 
