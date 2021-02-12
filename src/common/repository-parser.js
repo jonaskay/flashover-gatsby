2e64f4d85cceae947cba034a865b27d9039b6bf0
Joonas Kykkänen <jonaskay@iki.fi>
2021-02-12 21:15:37 +0200

    Add separate component for GitHub links

--- src/common/repository-parser.js
+++ src/common/repository-parser.js
@@ -1,23 +1,23 @@
+const Joi = require("joi")
+
 module.exports = class RepositoryParser {
   constructor(repository) {
+    const schema = Joi.object({
+      repository: Joi.string().pattern(
+        /^(?:git\+)?(?:https|http):\/\/\S+[^(\.git)](?:\.git)?$/i
+      ),
+    })
+    const { error } = schema.validate({ repository })
+    if (error) {
+      throw error
+    }
+
     this.repository = repository
   }
 
   url() {
-    this.validate()
-
     return this.repository.match(
       /^(?:git\+)?((?:https|http):\/\/\S+[^(\.git)])(?:\.git)?$/i
     )[1]
-  }
-
-  validate() {
-    if (
-      !/^(?:git\+)?(?:https|http):\/\/\S+[^(\.git)](?:\.git)?$/i.test(
-        this.repository
-      )
-    ) {
-      throw `${this.repository} is not a valid repository`
-    }
   }
 }
