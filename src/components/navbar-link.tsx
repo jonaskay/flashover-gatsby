8894a8b426ffc4bd78a688d71eaeac9af5522357
Joonas Kykkänen <jonaskay@iki.fi>
2021-02-19 20:38:46 +0200

    Fix archive link active class on paginated pages

--- src/components/navbar-link.tsx
+++ src/components/navbar-link.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
+const isRootPath = (path: string): boolean => path === "/"
+
 type NavbarLinkProps = {
   children: React.ReactNode
   className?: string
@@ -12,6 +14,7 @@ const NavbarLink: React.FC<NavbarLinkProps> = ({ children, className, to }) => (
     to={to}
     className={`mx-4 text-lg hover:text-black ${className ? className : ""}`}
     activeClassName="text-black"
+    partiallyActive={!isRootPath(to)}
   >
     {children}
   </Link>
