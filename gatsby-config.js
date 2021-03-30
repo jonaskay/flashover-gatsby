23873823ba5c0a1a47f42df92228cdfe5652a5e2
“Joonas <jonaskay@iki.fi>
2021-03-30 19:37:30 +0300

    Render twitter embeds correctly

--- gatsby-config.js
+++ gatsby-config.js
@@ -154,6 +154,7 @@ module.exports = {
         queries: require("./src/common/algolia-queries"),
       },
     },
+    `gatsby-plugin-twitter`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
